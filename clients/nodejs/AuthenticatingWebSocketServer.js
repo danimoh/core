5fc5f034f32e52ab0f5a7b62fda4654e472c199c
danimoh <github.danimoh@neverbox.com>
2017-07-25 17:35:02 -0600

    authentication

---
+++ clients/nodejs/AuthenticatingWebSocketServer.js
@@ -0,0 +1,34 @@
+const WebSocket = require('ws'); // https://github.com/websockets/ws
+const https = require('https');
+const fs = require('fs');
+
+
+class AuthenticatingWebSocketServer extends Nimiq.Observable {
+    static get EVENTS() {
+        return {
+            NEW_CONNECTION: 'new-connection'
+        };
+    }
+    constructor(port, sslKeyFile, sslCertFile, passwordFile) {
+        //const authSecret = fs.readFileSync(passwordFile);
+        const authSecret = 'fancypw';
+
+        const sslOptions = {
+            key: fs.readFileSync(sslKey),
+            cert: fs.readFileSync(sslCert)
+        };
+        const httpsServer = https.createServer(sslOptions, (req, res) => {
+            res.writeHead(200);
+            res.end('Nimiq NodeJS Remote API\n');
+        }).listen(port);
+        this._wss = new WebSocket.Server({server: httpsServer});
+        this._wss.on('connection', ws => {
+            const connection = new AuthenticatedConnection(ws, authSecret);
+            connection.on(AuthenticatedConnection.EVENTS.CONNECTION_ESTABLISHED,
+                () => this.fire(AuthenticatingWebSocketServer.EVENTS.NEW_CONNECTION, connection));
+        });
+    }
+}
+
+
+module.exports = AuthenticatingWebSocketServer;
