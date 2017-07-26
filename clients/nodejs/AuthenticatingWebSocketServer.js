55a1d7aa41bc093c97d30cf91c695b9b668e9658
danimoh <github.danimoh@neverbox.com>
2017-07-25 20:59:00 -0600

    authentication

--- clients/nodejs/AuthenticatingWebSocketServer.js
+++ clients/nodejs/AuthenticatingWebSocketServer.js
@@ -13,7 +13,7 @@ class AuthenticatingWebSocketServer extends Nimiq.Observable {
     }
     constructor(port, sslKeyFile, sslCertFile, authSecretFile) {
         super();
-        const authSecret = fs.readFileSync(authSecretFile, 'utf8');
+        const authSecret = fs.readFileSync(authSecretFile, 'utf8').trim();
         console.log('auth secret is', authSecret);
 
         const sslOptions = {
