6ab41f41ea49f62be80be0c6d294333f5fe70cf9
danimoh <github.danimoh@neverbox.com>
2017-07-30 10:09:25 -0600

    full api for miner, wallet, mempool

--- clients/remote-core/server/RemoteMinerAPI.js
+++ clients/remote-core/server/RemoteMinerAPI.js
@@ -14,13 +14,6 @@ class RemoteMinerAPI extends RemoteApiComponent {
     }
 
     /** @overwrite */
-    _isValidListenerType(type) {
-        const VALID_LISTENER_TYPES = [RemoteMinerAPI.MessageTypes.MINER_STARTED, RemoteMinerAPI.MessageTypes.MINER_STOPPED,
-            RemoteMinerAPI.MessageTypes.MINER_HASHRATE_CHANGED, RemoteMinerAPI.MessageTypes.MINER_BLOCK_MINED];
-        return VALID_LISTENER_TYPES.indexOf(type) !== -1;
-    }
-
-    /** @overwrite */
     getState() {
         return {
             address: this.$.miner.address.toHex(),
@@ -28,12 +21,45 @@ class RemoteMinerAPI extends RemoteApiComponent {
             working: this.$.miner.working
         };
     }
+
+    /** @overwrite */
+    handleMessage(connection, message) {
+        if (message.command === RemoteMinerAPI.Commands.MINER_START_WORK) {
+            this._startWork();
+            return true;
+        } else if (message.command === RemoteMinerAPI.Commands.MINER_STOP_WORK) {
+            this._stopWork();
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /** @overwrite */
+    _isValidListenerType(type) {
+        const VALID_LISTENER_TYPES = [RemoteMinerAPI.MessageTypes.MINER_STARTED, RemoteMinerAPI.MessageTypes.MINER_STOPPED,
+            RemoteMinerAPI.MessageTypes.MINER_HASHRATE_CHANGED, RemoteMinerAPI.MessageTypes.MINER_BLOCK_MINED];
+        return VALID_LISTENER_TYPES.indexOf(type) !== -1;
+    }
+
+    _startWork() {
+        this.$.miner.startWork();
+    }
+
+    _stopWork() {
+        this.$.miner.stopWork();
+    }
 }
+/** @enum */
+RemoteMinerAPI.Commands = {
+    MINER_START_WORK: 'miner-start-work',
+    MINER_STOP_WORK: 'miner-stop-work'
+};
 /** @enum */
 RemoteMinerAPI.MessageTypes = {
     MINER_STATE: 'miner',
-    MINER_STARTED: 'miner-started',
-    MINER_STOPPED: 'miner-stopped',
+    MINER_STARTED: 'miner-start',
+    MINER_STOPPED: 'miner-stop',
     MINER_HASHRATE_CHANGED: 'miner-hashrate-changed',
     MINER_BLOCK_MINED: 'miner-block-mined',
 };
